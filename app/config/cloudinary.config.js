0738257fd2d0b387c7029f0b90568415b54bd075
Carlos Carrascal <carlos.carrascal@tecsup.edu.pe>
2025-10-07 12:32:00 -0500

    fix: corregir versión de cloudinary para compatibilidad con multer-storage

--- app/config/cloudinary.config.js
+++ app/config/cloudinary.config.js
@@ -1,12 +1,14 @@
-import { v2 as cloudinary } from "cloudinary";
+import cloudinary from "cloudinary";
 
-// Configurar Cloudinary
-cloudinary.config({
+// Configurar Cloudinary v1
+const cloudinaryV1 = cloudinary.v2;
+
+cloudinaryV1.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default cloudinary;
+export default cloudinaryV1;
 
 
